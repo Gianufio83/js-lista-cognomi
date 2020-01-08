02d5cc733bbe5869ecf30de65fa54d47f0bd5369
Gianufio83 <fiore.gianuario@gmail.com>
2020-01-08 16:05:57 +0100

    prova lista ordinata alfabeticamente

--- js/script.js
+++ js/script.js
@@ -3,9 +3,17 @@
 var cognome = prompt('Inserisci il tuo cognome');
 // inseriscilo in un array con altri cognomi
 var listaCognomi = ['Rossi', 'Bianchi', 'Verdi', 'Scuri', 'Chiari'];
+while (cognome.length < 0) {
+  cognome = prompt('Inserisci il tuo cognome');
+}
 listaCognomi.push(cognome);
+listaCognomi.sort();
 console.log(listaCognomi);
-// stampa la lista ordinata alfabeticamente (in html ul > li).
 
+// stampa la lista ordinata alfabeticamente (in html ul > li).
+for (var i = 0; i < listaCognomi.length; i++) {
+  // console.log(listaCognomi[i]);
+  document.getElementById('list_surname').innerHTML +=  '<li>' + listaCognomi[i] + '</li>';
+}
 
 // Scrivi anche la posizione della lista in cui il nuovo utente si trova.
