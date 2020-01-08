b24b1055d0422e61d627715b3763ff40ee08b587
Gianufio83 <fiore.gianuario@gmail.com>
2020-01-08 14:46:39 +0100

    chiedi il cognome all'utente ed inseriscilo in un array con altri

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// alert('Ciao');
+// Chiedi all’utente il cognome
+var cognome = prompt('Inserisci il tuo cognome');
+// inseriscilo in un array con altri cognomi
+var listaCognomi = ['Rossi', 'Bianchi', 'Verdi', 'Scuri', 'Chiari'];
+listaCognomi.push(cognome);
+console.log(listaCognomi);
+// stampa la lista ordinata alfabeticamente (in html ul > li).
+
+
+// Scrivi anche la posizione della lista in cui il nuovo utente si trova.
